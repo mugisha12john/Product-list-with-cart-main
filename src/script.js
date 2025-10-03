ef2d2f0c471a93989a4d9293abe4ee125692cd9e
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2025-10-03 19:29:12 +0200

    feat: increment and decrement the item quantity

--- src/script.js
+++ src/script.js
@@ -6,15 +6,15 @@ function toggleCart(e) {
 
   btn.outerHTML = `
     <div class="w-36 absolute left-10 top-[220px] flex justify-around gap-2 p-2 text-white bg-[#c83910] font-bold rounded-full">
-      <button>
+      <button id="decrement-btn">
         <img
           src="/assets/images/icon-decrement-quantity.svg"
           alt="decrement quantity"
           class="w-5 h-5 border-white border-2 rounded-full p-[2px] cursor-pointer"
         />
       </button>
-      <p>1</p>
-      <button>
+      <p id="item-quantity">1</p>
+      <button id="increment-btn">
         <img
           src="/assets/images/icon-increment-quantity.svg"
           alt="increment quantity"
@@ -24,6 +24,24 @@ function toggleCart(e) {
     </div>
   `;
   btn.removeEventListener("click", toggleCart);
+  // increment and decrement item quantity
+  const incrementBtn = document.getElementById("increment-btn");
+  const decrementBtn = document.getElementById("decrement-btn");
+  const itemQuantity = document.getElementById("item-quantity");
+  let quantity = 1;
+  incrementBtn?.addEventListener("click", () => {
+    quantity += 1;
+    itemQuantity.textContent = quantity;
+  });
+  console.log(decrementBtn);
+  decrementBtn?.addEventListener("click", () => {
+    if (quantity > 1) {
+      quantity -= 1;
+      itemQuantity.textContent = quantity;
+    } else {
+      alert("Quantity cannot be less than 1");
+    }
+  });
 }
 
 function cardListItem(category, name, price, image, index) {
