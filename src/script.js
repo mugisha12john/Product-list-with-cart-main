961308abeb47829d50e9bf78f38bf37f6e6b80d6
Mugisha Jean Baptiste <johnbutista004@gmail.com>
2025-10-03 19:13:12 +0200

    feat: change btn content when you click on the add cart button

--- src/script.js
+++ src/script.js
@@ -1,18 +1,42 @@
 const data = await fetch("../data.json").then((res) => res.json());
 
 const productList = document.getElementById("product-list");
-console.log(productList);
-function cardListItem(category, name, price, image) {
-  return `<div " class="relative mt-2">
+function toggleCart(e) {
+  const btn = e.currentTarget;
+
+  btn.outerHTML = `
+    <div class="w-36 absolute left-10 top-[220px] flex justify-around gap-2 p-2 text-white bg-[#c83910] font-bold rounded-full">
+      <button>
+        <img
+          src="/assets/images/icon-decrement-quantity.svg"
+          alt="decrement quantity"
+          class="w-5 h-5 border-white border-2 rounded-full p-[2px] cursor-pointer"
+        />
+      </button>
+      <p>1</p>
+      <button>
+        <img
+          src="/assets/images/icon-increment-quantity.svg"
+          alt="increment quantity"
+          class="w-5 h-5 border-white border-2 rounded-full p-[2px] cursor-pointer"
+        />
+      </button>
+    </div>
+  `;
+  btn.removeEventListener("click", toggleCart);
+}
+
+function cardListItem(category, name, price, image, index) {
+  return `<div " class="relative mt-2" >
              <div>
               <img
                 src="${image}"
                 alt="${name}"
-                class="w-65 h-65 rounded-lg"
+                class="w-65 h-60 rounded-lg"
               />
 
-              <button
-                class="w-40 absolute left-10 top-[240px] flex justify-center items-center gap-2 p-2 text-[#260f08] border-2 border-[#87635a] bg-white font-bold rounded-full hover:cursor-pointer"
+              <button data-id="${index}" 
+                class="cart-btn w-40 absolute left-10 top-[220px] flex justify-center items-center gap-2 p-2 text-[#260f08] border-2 border-[#87635a] bg-white font-bold rounded-full hover:cursor-pointer"
               >
                 <img src="/assets/images/icon-add-to-cart.svg" alt="" /> Add to
                 Cart
@@ -28,11 +52,15 @@ function cardListItem(category, name, price, image) {
 }
 // productList.innerHTML = cardListItem();
 const array = JSON.parse(JSON.stringify(data));
-array.forEach((item) => {
+array.forEach((item, index) => {
   productList.innerHTML += cardListItem(
     item.category,
     item.name,
     item.price,
-    item.image.desktop
+    item.image.desktop,
+    index
   );
+});
+document.querySelectorAll(".cart-btn").forEach((btn) => {
+  btn.addEventListener("click", toggleCart);
 });
